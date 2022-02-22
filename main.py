0683973a6f56da52aa4075e60a31e296003f685d
pjsrcool <jpham079@ucr.edu>
2022-02-22 10:19:37 -0800

    added .gitignore file and set up discord bot code

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import discord
+from secret import token
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    print()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$b '):
+        await message.channel.send(message.author.mention + " Hi. I recieved you message")
+
+
 if __name__ == "__main__":
-    print("hello world")
+    client.run(token)
